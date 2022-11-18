450c41284375c6de751236954977d2dbe7be657a
Nathan Omeri <magembenath@gmail.com>
2022-11-18 11:16:02 -0800

    Regex to filter sender, receiver and flags

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 #this script filters transactions and returns the sender,
 #receiver and their address. using regex
 
-puts ARGV[0].scan(/(from:(\d+))/)
+puts ARGV[0].scan(/\[from:(\w+)\]\s\[to:(\w+)\]\s\[flags:(.*?)\]/).join(',')
