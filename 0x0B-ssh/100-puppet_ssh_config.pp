4c8d4477e867248ac42b0d7161a8c2049e79c52f
Nathan Omeri <magembenath@gmail.com>
2023-01-13 13:22:55 -0800

    Puppet manifest to set up config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,12 @@
 # Puppet manifest to set up ssh configuration file
-class ssh_config {
   file_line { 'use school':
-    ensure => present,
+    ensure => 'present',
     path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/school',
   }
 
   file_line { 'no_password_authentication':
-    ensure => present,
+    ensure => 'present',
     path   => '/etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
   }
-}
-
