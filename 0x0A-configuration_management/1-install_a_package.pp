11313028b13a097a2f6afca656c91cf360659196
Nathan Omeri <magembenath@gmail.com>
2024-03-23 20:30:16 +0000

    Removed exec, sticking with package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # this file executes a command to install flask
 
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => ['/usr/bin'],
-  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-}
-
-package { 'python3-pip':
-  ensure   => installed,
+package { 'python3-flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
