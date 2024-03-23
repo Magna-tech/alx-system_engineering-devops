35b693d365f6d7841390a6750bc15bd3e7eae519
Nathan Omeri <magembenath@gmail.com>
2024-03-23 20:23:58 +0000

    Added exec to install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # this file executes a command to install flask
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin'],
+  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
+package { 'python3-pip':
+  ensure   => installed,
 }
